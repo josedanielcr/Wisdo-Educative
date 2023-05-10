ef92283c4a4b71b5060953ceb731ea9fe0cb6b92
José Daniel Canales Rodríguez <jcanales@Sociedaddesegurosdevida.cr>
2023-05-10 11:32:20 -0600

    Entity structure added

---
+++ Wisdoeducative.Domain/common/BaseAuditableEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wisdoeducative.Domain.Entities;
+using Wisdoeducative.Domain.enums;
+
+namespace Wisdoeducative.Domain.Common
+{
+    public class BaseAuditableEntity : BaseEntity
+    {
+        public DateTime CreationDate { get; set; }
+        public DateTime ChangeDate { get; set; }
+        public EntityChangeTypes EntityChangeType { get; set; }
+        public int ModifiedByUserId { get; set; }
+        public User? ModifiedByUser { get; set; }
+    }
+}
---
+++ Wisdoeducative.Domain/entities/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wisdoeducative.Domain.Common;
+
+namespace Wisdoeducative.Domain.Entities
+{
+    public class User : BaseAuditableEntity
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+
+    }
+} 
